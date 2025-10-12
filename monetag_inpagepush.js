adc3a5b51497b3731981e26af141d36fa189a387
ishwar1961 <lishwar2007@gmail.com>
2025-10-12 08:11:11 +0530

    Added Monetag In-Page Push ad script (zone 10023934)

---
+++ monetag_inpagepush.js
@@ -0,0 +1,6 @@
+self.options = {
+    "domain": "3nbf4.com",   // Monetag delivery domain
+    "zoneId": 10023934       // Your In-Page Push Zone ID
+};
+self.lary = "";
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
